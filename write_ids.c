8b88028f9af28ae3a1192d6a99c319acf792e929
Griffin Rogers <gdr5137@psu.edu>
2024-09-13 21:49:06 -0400

    Updated Student ID and Git ID

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "GriffinRogersPSU";
+    char STUDENT_ID[] = "900177314";
 
     return 0 ;
 }
